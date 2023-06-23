0658efa7e8a20f23a3cab1d5fb948841cb80c947
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-06-23 13:34:24 +0300

    Начинаем проект

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+function App() {
+    
+    return (
+      <div className='app'>
+        Привет
+      </div>
+    );
+  }
+
+  export default App;
+  
