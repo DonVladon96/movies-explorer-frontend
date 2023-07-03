95ad900ac0199899da4c0f8876329556612c6be8
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-07-03 15:01:17 +0300

    add sigup route

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,41 +1,35 @@
-import React, { useState, useEffect, useContext } from "react";
 import "./Register.css";
 import logo from "../../images/logo.svg";
-import { Link, useNavigate  } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 function Register() {
 
   return(
     <section className="register">
-      <div className="register__box">
-        <div className="register__logo-box">
+      <div className="register__container">
+        <div className="register__logo-container">
           <Link to="/">
             <img src={logo} alt="Логотип" className="register__logo" />
           </Link>
         </div>
-
         <h2 className="register__title">Доброе пожаловать!</h2>
-
-        <form noValidate className="register__form" name="register-form" >
-          <div className="register__field">
+        <form className="register__form">
+          <div className="register__inputs-container">
             <label>
-              <label className="register__name">Имя</label>
+              <span className="register__name">Имя</span>
               <input className="register__input"/>
-             <div className="error__register">Введите имя</div>}
             </label>
             <label>
-              <label className="register__email">E-mail</label>
+              <span className="register__email">E-mail</span>
               <input className="register__input"/>
-             <div className="error__register">Введите email</div>}
             </label>
             <label>
               <label className="register__password">Пароль</label>
               <input className="register__input"/>
-              <div className="error__register">Неправильный пароль</div>}
             </label>
           </div>
-          <div className="register__button-box">
+          <div className="register__button-container">
             <button className="register__button" type="submit">Зарегистрироваться</button>
             <Link className="register__link" to="/signin">
               Ещё не зарегистрированы?
