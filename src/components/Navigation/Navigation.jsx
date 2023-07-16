5e29f000a543477f1f79bb083278d6ca9f79f294
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-07-16 23:17:45 +0400

    поработал с навигацией

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,37 @@
-import { Link } from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation({ isOpenBurger }) {
   return(
+
+
     <div className={`navigation ${isOpenBurger ? 'navigation_active' : ''}`}>
+
       <Link to='/' className='navigation__link navigation__link_home'>
         Главная
       </Link>
       <ul className='navigation__list'>
+      {/*Намучался с этим местом*/}
         <li>
-          <Link to='/movies' className='navigation__link'>
+          <NavLink
+            to="/movies"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : "navigation__link"
+            }
+          >
             Фильмы
-          </Link>
+          </NavLink>
         </li>
+
         <li>
-          <Link to='/saved-movies' className='navigation__link'>
+          <NavLink
+            to="/saved-movies"
+            className={({ isActive, isPending }) =>
+              isPending ? "pending" : isActive ? "active" : "navigation__link"
+            }
+          >
             Сохраненные фильмы
-          </Link>
+          </NavLink>
         </li>
       </ul>
       <Link to="/profile" className="navigation__profile">
