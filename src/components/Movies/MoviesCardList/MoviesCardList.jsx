ba67981b06241522aae7e89a5a734dc592d59c1b
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-07-17 01:06:26 +0400

    Правлю семантику 2

--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./MoviesCardList.css";
-import pic1 from "../../../images/pic__COLOR_pic.svg"
+import pic1 from "../../../images/pic__COLOR_pic.png"
 import pic2 from "../../../images/pic2.png"
 import pic3 from "../../../images/png3.png"
 import pic4 from "../../../images/png4.png"
@@ -20,20 +20,12 @@ function MoviesCardList() {
     <section className="movieCardList">
       <ul className="movie">
         <li className="movie__container">
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic1} alt="картина 33 слова о дизайне"/>
+          </Link>
 
-          <ul className='movie__list-style'>
-            <li>
-              <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-                <img className="movie__poster" src={pic1} alt="картина 33 слова о дизайне"/>
-              </Link>
-            </li>
-          </ul>
-
-          <li className="movie__caption-title">33 слова о дизайне</li>
-
-          <li>
-            <button className="movie__like-active"></button>
-          </li>
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
         </li>
 
         <li className="movie__time-line">1ч 47м</li>
@@ -41,20 +33,12 @@ function MoviesCardList() {
 
       <ul className="movie">
         <li className="movie__container">
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic2} alt="картина 33 слова о дизайне"/>
+          </Link>
 
-          <ul className='movie__list-style'>
-            <li>
-              <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-                <img className="movie__poster" src={pic2} alt="картина Киноальманах «100 лет дизайна»"/>
-              </Link>
-            </li>
-          </ul>
-
-          <li className="movie__caption-title">Киноальманах «100 лет дизайна»</li>
-
-          <li>
-            <button className="movie__like"></button>
-          </li>
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
         </li>
 
         <li className="movie__time-line">1ч 47м</li>
@@ -62,182 +46,131 @@ function MoviesCardList() {
 
       <ul className="movie">
         <li className="movie__container">
-          <li>
-            <ul className='movie__list-style'>
-              <li>
-                <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-                  <img className="movie__poster" src={pic3} alt="картина В погоне за Бенкси"/>
-                </Link>
-              </li>
-            </ul>
-          </li>
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic3} alt="картина 33 слова о дизайне"/>
+          </Link>
 
-          <li className="movie__caption-title">В погоне за Бенкси</li>
-
-          <li>
-            <button className="movie__like"></button>
-          </li>
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
         </li>
-        <p className="movie__time-line">1ч 47м</p>
-      </ul>
 
-      <ul className="movie">
-        <li className="movie__container">
-          <ul className='movie__list-style'>
-            <li>
-              <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-                <img className="movie__poster" src={pic4} alt="картина Баския: Взрыв реальности"/>
-              </Link>
-            </li>
-          </ul>
-
-          <li className="movie__caption-title">Баския: Взрыв реальности</li>
-          <li>
-            <button className="movie__like"></button>
-          </li>
-        </li>
         <li className="movie__time-line">1ч 47м</li>
       </ul>
 
       <ul className="movie">
         <li className="movie__container">
-          <ul className='movie__list-style'>
-            <li>
-              <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-                <img className="movie__poster" src={pic5} alt="картина Бег это свобода"/>
-              </Link>
-            </li>
-          </ul>
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic4} alt="картина 33 слова о дизайне"/>
+          </Link>
 
-          <li className="movie__caption-title">Бег это свобода</li>
-          <li>
-            <button className="movie__like"></button>
-          </li>
-
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
         </li>
+
         <li className="movie__time-line">1ч 47м</li>
       </ul>
 
       <ul className="movie">
         <li className="movie__container">
-          <ul className='movie__list-style'>
-            <li>
-              <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-                <img className="movie__poster" src={pic6} alt="Картина Книготворцы"/>
-              </Link>
-            </li>
-          </ul>
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic5} alt="картина 33 слова о дизайне"/>
+          </Link>
 
-          <li className="movie__caption-title">Книготорговцы</li>
-          <li>
-            <button className="movie__like-active"></button>
-          </li>
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
         </li>
+
         <li className="movie__time-line">1ч 47м</li>
       </ul>
 
       <ul className="movie">
         <li className="movie__container">
-          <ul className='movie__list-style'>
-            <li>
-              <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-                <img className="movie__poster" src={pic7} alt="Картина Когда я думаю о Гераминии ночью"/>
-              </Link>
-            </li>
-          </ul>
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic6} alt="картина 33 слова о дизайне"/>
+          </Link>
 
-          <li className="movie__caption-title">Когда я думаю о Германии ночью</li>
-          <li>
-            <button className="movie__like"></button>
-          </li>
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
         </li>
+
         <li className="movie__time-line">1ч 47м</li>
       </ul>
 
       <ul className="movie">
         <li className="movie__container">
-          <ul className='movie__list-style'>
-            <li>
-              <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-                <img className="movie__poster" src={pic8} alt="Картина Gimme Danger"/>
-              </Link>
-            </li>
-          </ul>
-          <li className="movie__caption-title">Gimme Danger: История Игги и The Stooge...</li>
-          <li>
-            <button className="movie__like"></button>
-          </li>
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic7} alt="картина 33 слова о дизайне"/>
+          </Link>
+
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
         </li>
+
         <li className="movie__time-line">1ч 47м</li>
       </ul>
 
       <ul className="movie">
         <li className="movie__container">
-          <ul className='movie__list-style'>
-            <li>
-              <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-                <img className="movie__poster" src={pic9} alt="Картина Дженис"/>
-              </Link>
-            </li>
-          </ul>
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic8} alt="картина 33 слова о дизайне"/>
+          </Link>
 
-          <li className="movie__caption-title">Дженис: Маленькая девочка грустит</li>
-          <li>
-            <button className="movie__like-active"></button>
-          </li>
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
         </li>
+
         <li className="movie__time-line">1ч 47м</li>
       </ul>
 
       <ul className="movie">
         <li className="movie__container">
-          <ul className='movie__list-style'>
-            <li>
-              <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-                <img className="movie__poster" src={pic10} alt="Картина Соберись перед прыжком"/>
-              </Link>
-            </li>
-          </ul>
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic9} alt="картина 33 слова о дизайне"/>
+          </Link>
 
-          <li className="movie__caption-title">Соберись перед прыжком</li>
-          <li>
-            <button className="movie__like-active"></button>
-          </li>
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
         </li>
+
         <li className="movie__time-line">1ч 47м</li>
       </ul>
 
       <ul className="movie">
         <li className="movie__container">
-          <ul className='movie__list-style'></ul>
-          <li>
-            <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-              <img className="movie__poster" src={pic11} alt="Картина Пи Джей Харви"/>
-            </Link>
-          </li>
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic10} alt="картина 33 слова о дизайне"/>
+          </Link>
 
-          <li className="movie__caption-title">Пи Джей Харви: A dog called money</li>
-          <li>
-            <button className="movie__like"></button>
-          </li>
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
         </li>
+
         <li className="movie__time-line">1ч 47м</li>
       </ul>
 
       <ul className="movie">
         <li className="movie__container">
-          <ul className='movie__list-style'>
-            <li>
-              <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
-                <img className="movie__poster" src={pic12} alt="Картина По волнам"/>
-              </Link>
-            </li>
-          </ul>
-          <li className="movie__caption-title">По волнам: Искусство звука в кино</li>
-          <li>
-            <button className="movie__like"></button>
-          </li>
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic11} alt="картина 33 слова о дизайне"/>
+          </Link>
+
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
         </li>
+
+        <li className="movie__time-line">1ч 47м</li>
+      </ul>
+
+      <ul className="movie">
+        <li className="movie__container">
+          <Link to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} target={"_blank"}>
+            <img className="movie__poster" src={pic12} alt="картина 33 слова о дизайне"/>
+          </Link>
+
+          <h3 className="movie__caption-title">33 слова о дизайне</h3>
+          <button className="movie__like-active"></button>
+        </li>
+
         <li className="movie__time-line">1ч 47м</li>
       </ul>
     </section>
