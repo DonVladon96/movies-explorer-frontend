4dfb287c9c08ed93b0c580be1e0d936831685762
DonVladon96 <Petrov.Vladik2010@yandex.com>
2023-07-26 17:33:04 +0400

    добавил свою апи

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./AboutMe.css";
-import Avatar from "../../../images/me.jpg";
+import Avatar from "../../../images/Iam.jpg";
 
 
 function AboutMe() {
@@ -14,8 +14,8 @@ function AboutMe() {
             <p className="aboutme__job">Фронтенд-разработчик, 27 лет</p>
             <p className="aboutme__biography">Я&nbsp;родился и&nbsp;живу в&nbsp;Ижевске, закончил факультет юриспруденции
               УдГУ. У&nbsp;меня есть девушка
-              и&nbsp;кот. Я&nbsp;люблю слушать музыку, а&nbsp;ещё увлекаюсь бегом. Недавно начал кодить. С 2022
-              года занимаюсь программированием. После того, как прошёл курс по&nbsp;веб-разработке,
+              и&nbsp;кот. Я&nbsp;люблю слушать музыку, а&nbsp;ещё увлекаюсь бегом. С 2022
+              года занимаюсь программированием. Люблю добавлять креатива во Frontend разработку. После того, как прошёл курс по&nbsp;веб-разработке,
               начал заниматься фриланс-заказами и&nbsp;ушёл с&nbsp;постоянной работы.
             </p>
             <a className="aboutme__github" href="https://github.com/DonVladon96" target="_blank"
